3e56c04e5495bde9fb3f8dcd8db6b5393200435b
gittjar <jarno.keranen@gmail.com>
2023-05-05 18:25:19 +0300

    done service and links to api

---
+++ src/app/place.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaceService {
+
+  constructor(private PlaceHTTP: HttpClient) { }
+
+  getPlace(): any {
+    const place = this.PlaceHTTP.get('https://api.codetabs.com/v1/proxy/?quest=https://api.hel.fi/linkedevents/v1/place/');
+    return place;
+    }
+
+
+}
+
+
---
+++ src/app/place/place.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+
+@Component({
+  selector: 'app-place',
+  templateUrl: './place.component.html',
+  styleUrls: ['./place.component.css']
+})
+export class PlaceComponent {
+
+}
