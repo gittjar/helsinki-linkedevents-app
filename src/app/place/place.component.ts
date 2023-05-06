98de28a1934021018559ea6719a131c1ff946602
gittjar <jarno.keranen@gmail.com>
2023-05-06 14:11:35 +0300

    points to map working from database

--- src/app/place/place.component.ts
+++ src/app/place/place.component.ts
@@ -23,16 +23,38 @@ export class PlaceComponent implements OnInit{
     center: { lat: 60.170345, lng: 24.952229 },
     zoom: 14,
   };
-
+/*
   markers: MarkerProperties[] = [
     { position: { lat: 60.169266, lng: 24.938164, title: 'Kauppakeskus Forum' }}, // Kauppakeskus Forum
-  ];
+  ];*/
 
   constructor (private placehttp: PlaceService){}
 
   ngOnInit(): void {
-    
+    this.getPlaceMarker();
   }
+/*
+  pleissi : any;
+  getPlaceMarker():void {
+    this.placehttp.getPlace().subscribe((data: any) => {
+      this.pleissi = data;
+    })
+  } */
+  pleissi: any;
+markers: any[] = [];
+
+getPlaceMarker(): void {
+  this.placehttp.getPlace().subscribe((data: any) => {
+    this.pleissi = data;
+    this.pleissi.data.forEach((place: any) => {
+      const marker = new google.maps.Marker({
+        position: { lat: place.position.coordinates[1], lng: place.position.coordinates[0] },
+        // map : 'GoogleMap' // replace "this.map" with a reference to your Google Map instance
+      });
+      this.markers.push(marker);
+    });
+  });
+}
 
 
 }
