415731d85ed042b2b0255b75d82fb194a2257812
gittjar <jarno.keranen@gmail.com>
2023-05-08 13:26:03 +0300

    tailwind installed and data moved to place component

--- src/app/place/place.component.ts
+++ src/app/place/place.component.ts
@@ -8,72 +8,107 @@ import { PlaceService } from '../place.service';
   styleUrls: ['./place.component.css']
 })
 export class PlaceComponent implements OnInit{
-/*
-  @ViewChild('GoogleMap', { static: true }) 
 
+  mapLoaded!: boolean;
   map!: google.maps.Map;
-
+  geocoder = new google.maps.Geocoder();
   infoWindow!: google.maps.InfoWindow;
-  mapOptions: google.maps.MapOptions = {
-    center: { lat: 60.170345, lng: 24.952229 },
-    zoom: 14,
-    
+  options: google.maps.MapOptions = {
+   // mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: true,
+  //  disableDefaultUI: true,
+  //  disableDoubleClickZoom: true,
+    center: {
+      lat: 60.16833266,
+      lng: 24.951496194,
+    },
+    zoom: 16,
   };
+
+ngOnInit() {
+  this.map = new google.maps.Map(
+    document.getElementById("map")!,
+    this.options
+  );
+  this.infoWindow = new google.maps.InfoWindow();
+  this.showContent('MyText');
+}
+
+markers = [] as any;
+
+constructor(private placeservice: PlaceService) {} 
+
+pleissi?: any;
+textid : string = "";
+
+DoSearch() {
+  console.log(this.textid);
+  this.showContent('MyText');
+  }
+
+  resetMap() {
   
-  textid: string = '';
-
- 
-*/
-  constructor (private placehttp: PlaceService){}
-
-  ngOnInit(): void {
-  // this.getPlaceMarker();
-   // this.infoWindow = new google.maps.InfoWindow();
+    
   }
 
 
+showContent(contentType: string) {
 
+  this.markers = []
 
+  let content: any = null
 
-/*
+  // getPlace in placeservice is configured to show text and it changes here what user gives.
+  if(contentType === "MyText") {
+    content = this.placeservice.getPlace(this.textid);
+  }
+  else {
+    console.error("unknown content type");
+    return
+  }
 
-  
-  pleissi?: any;
-  markers: any[] = [];
-getPlaceMarker(): void {
-  this.placehttp.getPlace().subscribe((data: any) => {
-    this.pleissi = data;
-    this.pleissi.data.forEach((place: any) => {
+  console.log("click")
 
-      const marker = new google.maps.Marker({
-        position: { lat: place?.position?.coordinates[1], lng: place?.position?.coordinates[0] },
-        // map : 'GoogleMap' // replace "this.map" with a reference to your Google Map instance
-        opacity : 1.3,
-        label : {text: place?.name?.fi, color: 'black', fontWeight: '700', fontFamily: 'Verdana', fontSize: '13px' },
-        title : place?.street_address?.fi + ', ' + place?.address_locality?.fi,
-        animation : google.maps.Animation.DROP,
-        icon: {url: '/assets/locationpin.png'},
-      });
-
-      const infoWindow = new google.maps.InfoWindow({
-        content: '' + place?.name?.fi + '' + place?.street_address?.fi + ', ' + place?.address_locality?.fi + '',
-        });
-
-
-      this.markers.push(marker);
-      //infowindow;
-      marker.addListener('click', () => {
-        infoWindow.open(this.map, marker);
-        });
-   
-      })
- 
-    });
+  content.subscribe((response: any) => {
     
-  };
-*/
+      let arr = response.data as Array<any>
 
+      arr.forEach((place: any) => {
+        this.pleissi = response;
+        let marker = new google.maps.Marker({
+          position: {
+            lat: place?.position?.coordinates[1],
+            lng: place?.position?.coordinates[0],
+          },
+          label : {text: place?.name?.fi, color: 'black', fontWeight: '700', fontFamily: 'Verdana', fontSize: '13px' },
+          title : place?.street_address?.fi + ', ' + place?.address_locality?.fi,
+          animation : google.maps.Animation.DROP,
+          icon: {url: '/assets/locationpin.png'},
+        });
+        
+
+
+        let markerContent = '<div class="infoscreen">' +
+                            place.name.fi + 
+                            `<p class="place_text">${place?.address_locality?.fi}</p>` + 
+                            '</div>'
+                           
+
+        
+
+        // To add the marker to the map, call setMap();
+        marker.setMap(this.map);
+        google.maps.event.addListener(marker, "click", () => {
+          let infowindow = new google.maps.InfoWindow();
+          infowindow.setContent(markerContent)
+          infowindow.open(this.map, marker);
+        });
+      });
+      
+    });
+  
 }
 
-  
 
+
+}
