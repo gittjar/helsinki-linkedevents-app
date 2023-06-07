c7bc3c27e7a4bd6880234105625bea89da78e8d4
gittjar <jarno.keranen@gmail.com>
2023-06-07 10:24:42 +0300

    dataconnections working

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+interface ImageResponse {
+  meta: {
+    count: number;
+    next: string;
+    previous: string;
+  };
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,6 +22,20 @@ export class ImageService {
   getImages(page: number): Observable<any> {
     const ImageUrl = this.imageHTTP.get<any>(this.IMGURL+page);
     return ImageUrl;
+  }
+
+  getImageData(): Observable<ImageResponse> {
+    return this.imageHTTP.get<ImageResponse>('https://api.codetabs.com/v1/proxy/?quest=https://api.hel.fi/linkedevents/v1/image/');
+  }
+
+  getPageNumber(url: string): number {
+    const match = url.match(/page=(\d+)/);
+    return match ? parseInt(match[1], 10) : 1;
+  }
+  
+  getMaxPageNumber(url: string): number {
+    const match = url.match(/page=(\d+)/);
+    return match ? parseInt(match[1], 10) : 1;
   }
 
 
--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../image.service';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit {
+
+  constructor(private http: ImageService ) {}
+
+  ngOnInit(): void {
+    this.getImageData(1);
+  }
+
+  images : any;
+  getImageData(page: number): void {
+    this.http.getImages(page).subscribe((data: any) => {
+      this.images = data;
+    })
+  }
+
+  newPageNumber = 1;
+  onPageChangePlus(): any {
+    this.increasePageNumber();
+    this.http.getImages(this.newPageNumber).subscribe((data: any) => {
+    this.images = data;
+    })
+    }
+    increasePageNumber(): void {
+      this.newPageNumber++;
+      }
 
 }
